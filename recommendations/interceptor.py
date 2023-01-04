c4f856163432546395b1500399d863b892f6dbe0
Spencer Ofwiti <maxspencer56@gmail.com>
2023-01-04 14:58:50 +0300

    Add interceptors

---
+++ recommendations/interceptor.py
@@ -0,0 +1,12 @@
+from grpc_interceptor import ServerInterceptor
+
+class ErrorLogger(ServerInterceptor):
+    def intercept(self, method, request, context, method_name):
+        try:
+            return method(request, context)
+        except Exception as e:
+            self.log_error(e)
+            raise
+
+    def log_error(self, e: Exception) -> None:
+        print(f'Error occurred: {e.message}')
